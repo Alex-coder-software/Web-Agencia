128df2d710c1f9bd9d8f47f7546eb45696d08a67
Alex_coder <162802805+Alex-coder-software@users.noreply.github.com>
2024-07-27 14:31:19 +0200

    Delete agencia_web/agencia_web directory

--- agencia_web/agencia_web/styles/fonts.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Font(Enum):
-    DEFAULT = "Roboto Condensed"
-    TITLE = "Roboto Condensed" # La misma fuente para todo
-
-class FontWeight(Enum):
-    LIGHT = "300"
-    MEDIUM = "500"
